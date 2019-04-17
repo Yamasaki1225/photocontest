6a0eb0dd566d267dd1304f7f6799484e3140d7c7
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2019-04-17 18:41:24 +0900

    【upgrade】各フォーム作成

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,18 +6,53 @@ class HelloController extends AppController {
     public $autoRender = false;  //自動レンダリングをon
 
     public function initialize(){
-      $this->name = 'Hello';
-      $this->autoRender = false;
+      // $this->name = 'Hello';
+      // $this->autoRender = false;
       $this->viewBuilder()->autoLayout(true);
       $this->viewBuilder()->Layout('Hello');
+      $this->set('footer', 'Hello\footer2');
+      $this->set('test', 'Hello\index');
     }
 
     public function index () {
+      $result = "";
+      if ($this->request->is('Post')){
+        $result = "<pre>※送信された情報<br/>";
+        foreach($this->request->data['HelloForm'] as $key => $val){
+          $v_str = '';
+          foreach($val as  $v){
+            $v_str .= $v . ' ';
+          }
+          $result .= $key . ' => ' . $v_str;
+        }
+      //   $result .= $key . " => " . $val;
+      //   }
+        $result .= "</pre>";
+      } else {
+        $result = "※なにか書いて送信してください。";
+      }
+      $this->set("result", $result);
       $this->viewBuilder()->autoLayout(true);  //自動レイアウトをon
-      $this->autoRender = true; //インスタンスが生成される度にinitializeが読み込まれる
-      $this->set('test', 'ヘッダーエレメント');
-      $n = rand(1, 2);
-      $this->set('footer','Hello\footer'.$n);
+      $this->autoRender = true; //viewファイルを使用するかどうか
+    }
+
+    public function sendForm() {
+      // $result = "送信された情報"."<br/>";
+      // debug($this->request->session());
+      // debug($this->request->query);
+      // foreach($this->request->query as $key => $val) {
+      //   $result .= $key . " => " . $val . "<br/>";
+      // }
+      $this->viewBuilder()->autoLayout(true);
+      $this->autoRender = true;
+      $str = $this->request->data['text1'];
+      $result = "";
+      if ($str != ""){
+        $result = "you type:".$str;
+      } else {
+        $result = "empty.";
+      }
+      $this->set("result", h($result));
     }
 
     public function other() {
--- src/Template/Hello/index.ctp
+++ src/Template/Hello/index.ctp
@@ -6,7 +6,44 @@
 </head>
 <body>
 <h1>サンプル見出し</h1>
-  <p>こんにちは！これは、CakePHPのサンプルです。<p>
+<p>
+<?=$result; ?>
+</p>
+<?=$this->Form->create(null, ['type'=>'post', 'url'=>['controller'=>'Hello', 'action'=>'index']]) ?>
+  <?=$this->Form->select('HelloForm.select1', 
+  [
+    'PC'=>[
+      'ウィンドウズ'=>'Windows',
+      'リナックス'=>'Linux',
+      'マックOS'=>'MacOS X'
+    ],
+    'mobile'=>[
+      'アンドロイド'=>'android',
+      'アイフォン'=>'iPhone',
+      'ガラケー'=>'cellphone'
+    ]
+  ],
+  ['size'=>4, 'multiple'=>true,'empty'=>'項目を選んでください']);?>
+  <?=$this->Form->submit("送信") ?>
+<?=$this->Form->end(); ?>
+  <!-- HelloControllerのsendFormアクションに値を渡す -->
+<!-- <form method="post" action="hello/sendForm"> -->
+  <!-- <input type="hidden" name="check1" value="off">
+  <input type="hidden" name="radio1" value="off">
+  <input type="checkbox" name="check1" id="c1" />
+    <label for="c1">チェック</label><br />
+  <input type="radio" name="radio1" id="1" value="No.1">
+    <label for="r2">ラジオ１</label><br />
+  <input type="radio" name="radio1" id="2" value="No.2">
+    <label for="r2">ラジオ2</label><br />
+  <select name="select1">
+    <option value="Windows">Windows</option>
+    <option value="Linux">Linux</option>
+    <option value="Mac">Mac</option>
+  </select> -->
+  <!-- <input type="text" name="text1" />
+  <input type="submit" />
+</form> -->
 <br><br/>
 <?=date('Y/m/d',time()) ?>
 </body>
