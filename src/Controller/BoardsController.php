472a2b5f2391bb0e8957a51944d46e2ae8deb4b7
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2019-05-23 16:25:29 +0900

    create_view_file

--- src/Controller/BoardsController.php
+++ src/Controller/BoardsController.php
@@ -3,6 +3,20 @@ namespace App\Controller;
 
 class BoardsController extends AppController {
     public function index() {
+        /**
+         * $this->BoardsはBoardsクラスのインスタンスが入っている。
+         * つまりテーブルクラスのインスタンスがメンバ変数に追加される。
+         * find('all')でテーブルにあるレコードを全て取得する
+         */
+        $data = $this->Boards->find('all');
+        $this->set('data', $data);
+    }
+    public function test() {
+        /**
+         * $this->BoardsはBoardsクラスのインスタンスが入っている。
+         * つまりテーブルクラスのインスタンスがメンバ変数に追加される。
+         * find('all')でテーブルにあるレコードを全て取得する
+         */
         $data = $this->Boards->find('all');
         $this->set('data', $data);
     }
---
+++ src/Template/Boards/test.ctp
@@ -0,0 +1,19 @@
+<h1>Databaseサンプル</h1>
+<table>
+<tr>
+    <th>ID</th>
+    <th>NAME</th>
+    <th>TITLE</th>
+    <th>CONTENT</th>
+</tr>
+<?php
+$arr = $data->toArray();
+for($i = 0; $i < count($arr); $i++){
+    echo $this->Html->tablecells(
+    $arr[$i]->toArray(),
+    ['style'=>'background-color:#f0f0f0'],
+    ['style'=>'font-weight:bold'],
+    true);
+}
+?>
+</table>
